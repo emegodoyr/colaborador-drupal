4fc1ab23b32f7416d862d39f4d4d2640a7b8db7d
Moisés Godoy <mgodoy@dl.cl>
2016-08-31 11:04:48 -0300

    validacion para no mostar eventos a administradores del sistema

--- app/sites/all/modules/custom/cards/cards.theme.tpl.php
+++ app/sites/all/modules/custom/cards/cards.theme.tpl.php
@@ -3,7 +3,13 @@
 
 echo '<div class="row icons">';
 foreach($info as $entry) {
-
+    if($entry[12] > 0) {
+        $haveContent = true;
+        echo '<a href="/eventos/contenidosEvento?idEvento='.$entry[0].'&idPersona='.$idPersona.'">';
+    } else {
+        $haveContent = false;
+        echo '<a href="#">';
+    }
     echo '
 
 <section class="span2" style="border:1px solid lightgrey;padding-top:0; margin-bottom: 15px; border-radius: 5px 5px 5px 5px;">
@@ -12,25 +18,23 @@ foreach($info as $entry) {
         <label style=" color: whitesmoke ;height: 30px; margin-left: 5px; padding:5px; margin-right: 5px;overflow:hidden; white-space:nowrap; text-overflow: ellipsis";>'.$entry[1].'</label>
     </header>
 
-    <content style="text-align:center;">
+    <article style="text-align:center;background-image:url('.$absolut.'/img/back-card-default.png)">
         
-         <label style="height: 80px;padding:0;margin:0;background-image:url('.$absolut.'/img/back-card-default.png)">';
+         <label style="height: 80px;padding:0;margin:0;">';
        
-            if($entry[12] > 0) {
-               echo '<a href="/eventos/contenidosEvento?idEvento='.$entry[0].' & idPersona='.$idPersona.'">
-                        <i style = "color:mediumseagreen;font-size:80px;" class="icon-play-circle"></i>
-                    </a>';
+            if($haveContent) {
+               echo '<i style = "color:mediumseagreen;font-size:80px;" class="icon-play-circle"></i>';
             }
 	    echo '</label> 
         
      
-    </content>
+    </article>
 
     <footer style="border-top: 1px solid lightgrey; height: 20px;">
         <i style="color:dodgerblue;float:right;font-size:15px; vertical-align:center;" class="icon-info-sign"></i>
     </footer>
     
-</section>';
+</section></a>';
 
 };
 
--- app/sites/all/modules/custom/eventos/js/modalCurso.js
+++ app/sites/all/modules/custom/eventos/js/modalCurso.js
@@ -1,5 +1,5 @@
 (function($) {
-
+    $ = jQuery;
   var cursoModal = function(){
     $(".cursoModal").append('<div class="modal fade"  id="modalCursoPlayer" style="overflow-y:hidden"  tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog-expanded" ><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">X</button><h4 class="modal-title">&nbsp;</h4> </div><div class="modal-body-extended"><iframe id="frameCurso"  width="100%" height="100%" frameborder="0" allowtransparency="true"></iframe></div></div></div></div>');
 };
