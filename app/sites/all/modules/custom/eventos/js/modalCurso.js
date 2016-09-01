6d6b05e380ec190736f99020e31eab6d18f0e60c
Moisés Godoy <mgodoy@dl.cl>
2016-09-01 09:15:45 -0300

    refatorizacion -estrategica- modal contenido de eventos

--- app/sites/all/modules/custom/eventos/js/modalCurso.js
+++ app/sites/all/modules/custom/eventos/js/modalCurso.js
@@ -1,34 +1,44 @@
 (function($) {
     $ = jQuery;
-  var cursoModal = function(){
-    $(".cursoModal").append('<div class="modal fade"  id="modalCursoPlayer" style="overflow-y:hidden"  tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog-expanded" ><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">X</button><h4 class="modal-title">&nbsp;</h4> </div><div class="modal-body-extended"><iframe id="frameCurso"  width="100%" height="100%" frameborder="0" allowtransparency="true"></iframe></div></div></div></div>');
-};
+    var cursoModal = function(){
+        var modal = $(".cursoModal").append('<div class="modal fade"  id="modalCursoPlayer" style="overflow-y:hidden"  tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog-expanded" ><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">X</button><h4 class="modal-title">&nbsp;</h4> </div><div class="modal-body-extended"><iframe id="frameCurso"  width="100%" height="100%" frameborder="0" allowtransparency="true"></iframe></div></div></div></div>');
+        modal.css({
+            width: '100%',
+            heigth : '100%',
+            zIndex: '-9999',
 
-cursoModal.prototype.show = function(URL) {
-console.log(URL);
-    $(".modal-header").hide();
-  $("#frameCurso").attr("src", URL);
-  $("#modalCursoPlayer").modal({backdrop: 'static'});
-  $("#modalCursoPlayer").modal("show");
-};
+        });
+    };
 
-cursoModal.prototype.showUrl = function (URL) {
-    $(".modal-header").show();
-    $("#frameCurso").attr("src", URL);
-	$("#modalCursoPlayer").modal({backdrop: 'static'});
-    $("#modalCursoPlayer").modal("show");
-};
+    cursoModal.prototype.show = function(URL) {
+        $(".modal-header").hide();
+        $("#frameCurso").attr("src", URL);
+        $("#modalCursoPlayer").modal({backdrop: 'static'});
+        $("#modalCursoPlayer").modal("show");
+        $(".cursoModal").css({
+            zIndex: '9999',
+        });
+    };
+
+    cursoModal.prototype.showUrl = function (URL) {
+        $(".modal-header").show();
+        $("#frameCurso").attr("src", URL);
+        $("#modalCursoPlayer").modal({backdrop: 'static'});
+        $("#modalCursoPlayer").modal("show");
+    };
 
 
 cursoModal.prototype.hide = function(){
 
-  
-  $("#modalCursoPlayer").modal("hide");
-  $("#frameCurso").attr("src","about:blank");
+    $(".cursoModal").css({
+        zIndex: '-9999',
+    });
+    $("#modalCursoPlayer").modal("hide");
+    $("#frameCurso").attr("src","about:blank");
 
 };
 
-$(document).ready(function(){
-	curso = new cursoModal();
-});
+    $(document).ready(function(){
+        curso = new cursoModal();
+    });
 })(jQuery);
